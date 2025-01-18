9b3d54fe5a1b8c8160887e6fcac660a0ade70a5c
rohitDhanani <dhanani51@gmail.com>
2025-01-18 17:37:45 +0530

    changed hero image path

--- src/components/HomeComponents/HeroSec.jsx
+++ src/components/HomeComponents/HeroSec.jsx
@@ -12,7 +12,7 @@ const HeroSec = () => {
       }
     }, []);
   return (
-    <div ref={targetRef} className="bg-[linear-gradient(rgba(0,0,0,0.5),rgba(0,0,0,0.5)),url('src/assets/img3.jpg')] bg-cover bg-center z-0 h-screen flex flex-col justify-center gap-10 items-center text-white">
+    <div ref={targetRef} className="bg-[linear-gradient(rgba(0,0,0,0.5),rgba(0,0,0,0.5)),url('/img3.jpg')] bg-cover bg-center z-0 h-screen flex flex-col justify-center gap-10 items-center text-white">
       <div className='flex flex-col justify-center items-center gap-1 text-white'>
 
         <h1 className='text-5xl font-bold px-24 bg-gradient-to-r from-[#2563EB]  to-[#93C5FD] inline-block text-transparent bg-clip-text'>{PERSONAL_INFO.firm}</h1>
